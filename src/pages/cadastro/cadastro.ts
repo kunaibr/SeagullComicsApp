e9e0b4b6cbea4387d995f7e18ce4f9eeab9d76e4
Kunaibr <46764416+kunaibr@users.noreply.github.com>
2019-05-14 16:37:38 -0300

    cadastro firebase 50%

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { ServidorProvider } from '../../providers/servidor/servidor';
 import { Http } from '@angular/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ValidateConfirmPassword } from '../../validators/confirmPassword';
 
 @IonicPage()
 @Component({
@@ -10,12 +12,14 @@ import { Http } from '@angular/http';
 })
 export class CadastroPage {
 
+  registerForm: FormGroup;
+
   usuarios: any;
 
-  nome:String = "";
-  senha:String = "";
-  email:String = "";
-  senhaComfirm:String = "";
+  // nome:String = "";
+  // senha:String = "";
+  // email:String = "";
+  // senhaComfirm:String = "";
 
   constructor(
     public navCtrl: NavController, 
@@ -23,65 +27,76 @@ export class CadastroPage {
     public servidor:ServidorProvider,
     public http: Http,
     public toastCtrl: ToastController,
+    public formbuilder: FormBuilder,
     ) {
-      
+
+      this.registerForm = this.formbuilder.group({
+        nome: [null,[Validators.required, Validators.minLength(5)]],
+        email: [null,[Validators.required, Validators.email]],
+        senha: [null,[Validators.required, Validators.minLength(5)]],
+        senhaConfirm: [null,[Validators.required, Validators.minLength(5), ValidateConfirmPassword]],
+      });
   }
 
   EfetuarCadastro(){  
-    if(this.nome == "" || 
-      this.senha == "" || 
-      this.email == "" || 
-      this.senhaComfirm == ""){
+
+console.log(this.registerForm.value);
+
+    //Antigo
+    // if(this.nome == "" || 
+    //   this.senha == "" || 
+    //   this.email == "" || 
+    //   this.senhaComfirm == ""){
        
-          let toast = this.toastCtrl.create({
-            message: "Preencha todos os campos!",
-            duration: 3000,
-          })
+    //       let toast = this.toastCtrl.create({
+    //         message: "Preencha todos os campos!",
+    //         duration: 3000,
+    //       })
 
-          toast.present();
+    //       toast.present();
       
-    }else if(this.senhaComfirm != this.senha){
+    // }else if(this.senhaComfirm != this.senha){
      
-      let toast = this.toastCtrl.create({
-        message: "As duas senhas não são iguais!",
-        duration: 3000,
-      })
-      toast.present();
+    //   let toast = this.toastCtrl.create({
+    //     message: "As duas senhas não são iguais!",
+    //     duration: 3000,
+    //   })
+    //   toast.present();
 
-    }else{
-      let body ={
-        nome: this.nome,
-        email: this.email,
-        senha: this.senha, 
-        aksi: 'add_register',
-      };
+    // }else{
+    //   let body ={
+    //     nome: this.nome,
+    //     email: this.email,
+    //     senha: this.senha, 
+    //     aksi: 'add_register',
+    //   };
    
-      //ERRO
-      this.servidor.PostData(body,'register.php').subscribe((data) =>{
+    //   //ERRO
+    //   this.servidor.PostData(body,'register.php').subscribe((data) =>{
 
-         var alertpesan = data.msg;
+    //      var alertpesan = data.msg;
         
          
-         if(data.sucess){
+    //      if(data.sucess){
            
-           this.navCtrl.pop();         
+    //        this.navCtrl.pop();         
 
-           let toast = this.toastCtrl.create({
-            message: "Conta criada com sucesso!",
-            duration: 3000,
-          })
+    //        let toast = this.toastCtrl.create({
+    //         message: "Conta criada com sucesso!",
+    //         duration: 3000,
+    //       })
     
-          toast.present();
-          }else{
-            let toast = this.toastCtrl.create({
-              message: alertpesan,
-              duration: 3000,
-            })
+    //       toast.present();
+    //       }else{
+    //         let toast = this.toastCtrl.create({
+    //           message: alertpesan,
+    //           duration: 3000,
+    //         })
       
-            toast.present();
-          }
-      });   
-    }
+    //         toast.present();
+    //       }
+    //   });   
+    // }
     
     
   }
